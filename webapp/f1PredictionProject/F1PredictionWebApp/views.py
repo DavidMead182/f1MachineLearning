2e94e54858060add7c0ee50252b17125f16a2a14
David Mead <2652016m@student.gla.ac.uk>
2024-12-19 18:00:24 +0000

    Displaying data from FastF1 Api

--- webapp/f1PredictionProject/F1PredictionWebApp/views.py
+++ webapp/f1PredictionProject/F1PredictionWebApp/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
+from .helper_functions import *
+import fastf1 as ff1
 
 # Create your views here.
 def index(request):
     context = {}
-    return render(request, "index.html", context)
+    last_race_id, year, schedule = most_recent_race()
+    session = ff1.get_session(year, last_race_id, 'R')
+    session.load()
+    df = pd.DataFrame(session.results)
+    results = df[['Position', 'Abbreviation']]
+    context = {"results": results.to_dict(orient='records')}
+    return render(request, 'index.html', context)
 
 def login(request):
     context = {}
