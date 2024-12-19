aa9a1694e70de7d48d604a3fc9dec2f472d345d7
David Mead <2652016m@student.gla.ac.uk>
2024-12-19 03:42:32 +0000

    creating authentication pages

--- webapp/f1PredictionProject/F1PredictionWebApp/views.py
+++ webapp/f1PredictionProject/F1PredictionWebApp/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import HttpResponse, render
+from django.shortcuts import render
 
 # Create your views here.
 def index(request):
     context = {}
     return render(request, "index.html", context)
+
+def login(request):
+    context = {}
+    return render(request, "registration/login.html", context)
+
+def signup(request):
+    context = {}
+    return render(request, "registration/signup.html", context)
